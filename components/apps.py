4648456d1e2189a877f609b7869656ef22569b76
publishername <subashghimire325@gmail.com>
2025-12-06 13:03:25 +0545

    chore(component): Use component in password reset and confirm form

--- components/apps.py
+++ components/apps.py
@@ -6,5 +6,8 @@ class ComponentsConfig(AppConfig):
     name = "components"
 
     def ready(self):  # noqa: PLR6301
+        import components.components.email_input  # noqa: F401
         import components.components.password_input  # noqa: F401
         import components.components.select_button  # noqa: F401
+        import components.components.text_input  # noqa: F401
+        import components.components.textarea_input  # noqa: F401
