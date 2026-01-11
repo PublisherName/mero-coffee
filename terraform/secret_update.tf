08051c52923b43677559f5b42a7b3f923353d843
publishername <subashghimire325@gmail.com>
2026-01-11 22:17:40 +0545

    feat(deployment): Force deployment

--- terraform/secret_update.tf
+++ terraform/secret_update.tf
@@ -1,15 +1,12 @@
+#TODO: Remove the wildcard
 resource "aws_secretsmanager_secret_version" "mc_app_secret_updated" {
   secret_id = data.aws_secretsmanager_secret.mc_app_secret.id
   secret_string = jsonencode(merge(
     jsondecode(data.aws_secretsmanager_secret_version.mc_app_secret_current.secret_string),
     {
-      DB_HOST              = module.db.db_instance_address
-      DB_PORT              = tostring(module.db.db_instance_port)
-      DB_NAME              = module.db.db_instance_name
-      DB_USER              = module.db.db_instance_username
-      DB_PASSWORD          = jsondecode(data.aws_secretsmanager_secret_version.rds_master_password.secret_string)["password"]
-      DJANGO_ALLOWED_HOSTS = var.domain_name != "" ? "${var.domain_name},.${var.domain_name},127.0.0.1" : "${aws_lb.mc_alb.dns_name},127.0.0.1"
-      ALLOWED_CIDR_NETS    = join(",", module.vpc.private_subnets_cidr_blocks)
+      DATABASE_URL         = "postgresql://${module.db.db_instance_username}:${jsondecode(data.aws_secretsmanager_secret_version.rds_master_password.secret_string)["password"]}@${module.db.db_instance_address}:${module.db.db_instance_port}/${module.db.db_instance_name}"
+      DJANGO_ALLOWED_HOSTS = var.domain_name != "" ? "${var.domain_name},.${var.domain_name},*" : "*"
+      ALLOWED_CIDR_NETS    = "${var.vpc_cidr}"
       SITE_BASE_URL        = var.domain_name != "" ? "https://${var.domain_name}" : "http://${aws_lb.mc_alb.dns_name}"
     }
   ))
