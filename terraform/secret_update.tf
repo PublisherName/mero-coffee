4c2618d784a161f3497c551a3e24f83c8233228a
publishername <subashghimire325@gmail.com>
2026-01-11 19:25:07 +0545

    chore(secret): Update secrets hosts

--- terraform/secret_update.tf
+++ terraform/secret_update.tf
@@ -3,12 +3,14 @@ resource "aws_secretsmanager_secret_version" "mc_app_secret_updated" {
   secret_string = jsonencode(merge(
     jsondecode(data.aws_secretsmanager_secret_version.mc_app_secret_current.secret_string),
     {
-      DB_HOST     = module.db.db_instance_address
-      DB_PORT     = tostring(module.db.db_instance_port)
-      DB_NAME     = module.db.db_instance_name
-      DB_USER     = module.db.db_instance_username
-      DB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.rds_master_password.secret_string)["password"]
+      DB_HOST              = module.db.db_instance_address
+      DB_PORT              = tostring(module.db.db_instance_port)
+      DB_NAME              = module.db.db_instance_name
+      DB_USER              = module.db.db_instance_username
+      DB_PASSWORD          = jsondecode(data.aws_secretsmanager_secret_version.rds_master_password.secret_string)["password"]
+      DJANGO_ALLOWED_HOSTS = "${aws_lb.mc_alb.dns_name},10.0.0.0/16"
+      SITE_BASE_URL        = "http://${aws_lb.mc_alb.dns_name}"
     }
   ))
-  depends_on = [module.db]
+  depends_on = [module.db, aws_lb.mc_alb]
 }
