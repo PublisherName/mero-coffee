e6408cbe03ab8817d9e9d4cdb16f733f43eb4fad
publishername <subashghimire325@gmail.com>
2026-01-21 13:15:09 +0545

    feat(tests): Add PayPal payment test cases

--- apps/payments/tests/__init__.py
+++ apps/payments/tests/__init__.py
@@ -1,5 +1,11 @@
+from .test_paypal import PayPalPaymentTestCase
 from .test_stripe import StripePaymentTestCase
 from .test_withdrawal_form import WithdrawalFormTests
 from .test_withdrawal_view import WithdrawalViewTests
 
-__all__ = ["WithdrawalFormTests", "WithdrawalViewTests", "StripePaymentTestCase"]
+__all__ = [
+    "WithdrawalFormTests",
+    "WithdrawalViewTests",
+    "StripePaymentTestCase",
+    "PayPalPaymentTestCase",
+]
