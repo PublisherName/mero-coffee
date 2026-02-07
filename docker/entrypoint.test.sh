e24e98081e69ac54b69ca8c3d4bcad921a163dc2
publishername <subashghimire325@gmail.com>
2026-02-07 20:10:23 +0545

    feat(command): Add command to seed group based roles

--- docker/entrypoint.test.sh
+++ docker/entrypoint.test.sh
@@ -4,5 +4,6 @@ uv run manage.py migrate
 uv run manage.py loaddata fixtures/nepal_cities_light.json
 uv run manage.py seed_payment_gateways
 uv run manage.py seed_email_templates
+uv run manage.py seed_groups
 uv run manage.py collectstatic --no-input
 uv run manage.py test -v 3
